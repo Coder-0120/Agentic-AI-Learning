1fc2b80bc6ddbe4b5484133b4ca777999b2ecb52
Anshul Verma <av4862187@gmail.com>
2026-06-16 18:05:41 +0530

    Learns stream response

--- day1/index.js
+++ day1/index.js
@@ -5,9 +5,22 @@ const ai=new GoogleGenAI({
     apiKey:process.env.GEMINI_API_KEY,
 });
 
-const response=await ai.models.generateContent({
-    model: "gemini-3-flash-preview",
-    contents:"Define MERN STACK"
-});
+// const response=await ai.models.generateContent({
+//     model: "gemini-3-flash-preview",
+//     contents:"Define MERN STACK in 20 words only "
+// });
 
-console.log(response.text);
+// console.log(response.text); // one time response when done fully
+
+// for stream responses ,means giving side by side during generating 
+
+
+// const stream=await ai.models.generateContentStream({
+//     model: "gemini-3-flash-preview",
+//     contents:"Define MERN STACK in 20 words only "
+// });
+
+// for await (const chunk of stream){
+//     process.stdout.write(chunk.text);
+// }
+
