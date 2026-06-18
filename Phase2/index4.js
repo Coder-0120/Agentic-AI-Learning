14261f04cfcd99e50fbbee4821c9b30bdfec62e2
Anshul Verma <av4862187@gmail.com>
2026-06-18 21:46:26 +0530

    learns to generate imagge

---
+++ Phase2/index4.js
@@ -0,0 +1,31 @@
+// Image generation 
+import { GoogleGenAI } from "@google/genai";
+import * as fs from "node:fs";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function main() {
+    const ai = new GoogleGenAI({
+        apiKey: process.env.GEMINI_API_KEY,
+    });
+  const prompt =
+    "Create a picture of a person doing coding in gemini theme";
+
+  const response = await ai.models.generateContent({
+model: "gemini-2.5-flash-image",
+    contents: prompt,
+  });
+  console.log("Full response is :",response);
+  for (const part of response.candidates[0].content.parts) {
+    if (part.text) {
+      console.log(part.text);
+    } else if (part.inlineData) {
+      const imageData = part.inlineData.data;
+      const buffer = Buffer.from(imageData, "base64");
+      fs.writeFileSync("gemini-native-image.png", buffer);
+      console.log("Image saved as gemini-native-image.png");
+    }
+  }
+}
+
+main();
