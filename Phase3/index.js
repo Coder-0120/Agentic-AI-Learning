afafe9befbeb23d3c24ce9a5d3294e083ddb9fd0
Anshul Verma <av4862187@gmail.com>
2026-06-20 12:55:55 +0530

    RAG FUNDAMENTALS: Learn to calculate cosine similarity b/w 2 emb

--- Phase3/index.js
+++ Phase3/index.js
@@ -20,8 +20,31 @@ const emb2 = await ai.models.embedContent({
   model: "gemini-embedding-001",
   contents: text2
 });
-console.log("Some vectors of embedding vectors for text1 : ",
-  emb1.embeddings[0].values.slice(0, 5)
+// console.log("Some vectors of embedding vectors for text1 : ",
+//   emb1.embeddings[0].values.slice(0, 5)
+// );
+// console.log("numbers are present in the embedding vector for text1 : ",emb1.embeddings[0].values.length);
+// console.log("numbers are present in the embedding vector for text2 : " ,emb2.embeddings[0].values.length);
+
+//-------------------------------------------------------//-------------------------------------------------------
+// Calculate Cosine Similarity
+function cosineSimilarity(a, b) {
+  let dot = 0;
+  let magA = 0;
+  let magB = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    dot += a[i] * b[i];
+    magA += a[i] * a[i];
+    magB += b[i] * b[i];
+  }
+
+  return dot / (Math.sqrt(magA) * Math.sqrt(magB));
+}
+
+const similarity = cosineSimilarity(
+  emb1.embeddings[0].values,
+  emb2.embeddings[0].values
 );
-console.log("numbers are present in the embedding vector for text1 : ",emb1.embeddings[0].values.length);
-console.log("numbers are present in the embedding vector for text2 : " ,emb2.embeddings[0].values.length);
+
+console.log(similarity);
